cab77a5f0795382b5172cedda1964ac0ce61287b
Dewin154 <peterokruhlica@gmail.com>
2025-04-26 11:11:34 +0200

    Conversion of variables
    - converted variables year, price_in_euro, power_kw, power_ps, mileage_in_km from factor to numeric

--- Skript.R
+++ Skript.R
@@ -7,3 +7,13 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 data <- read.csv("data.csv", header=TRUE, sep=",", fill=TRUE, stringsAsFactors = TRUE)
 
 summary(data)
+
+#counts the number of cars that have in description column the word "unfallfrei"
+anzahl_unfallfrei <- sum(grepl("unfallfrei", data$offer_description))
+
+#correct conversion of variables to numeric
+data$year <- as.numeric(gsub(",", ".", as.character(data$year)))
+data$price_in_euro <- as.numeric(gsub(",", ".", as.character(data$price_in_euro)))
+data$power_kw <- as.numeric(gsub(",", ".", as.character(data$power_kw)))
+data$power_ps <- as.numeric(gsub(",", ".", as.character(data$power_ps)))
+data$mileage_in_km <- as.numeric(gsub(",", ".", as.character(data$mileage_in_km)))
