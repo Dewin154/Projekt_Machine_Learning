6f19c56f06ef3432a72a7db4b11889af4990e947
Nico <ni.dilger@gmail.com>
2025-06-02 13:57:31 +0200

    reduced tuning to get shorter running time

--- svm.R
+++ svm.R
@@ -1,14 +1,17 @@
 # install.packages("e1071")
 library(e1071)
 
-cc <- seq(-5,10,1)    # für mögliche Werte von "Cost" (Tuningparameter)
-cg <- seq(-3,1,0.5)   # für mögliche werte von "gamma" (Tuningparameter)
+#cc <- seq(-5,10,1)    # für mögliche Werte von "Cost" (Tuningparameter)
+#cg <- seq(-3,1,0.5)   # für mögliche werte von "gamma" (Tuningparameter)
+cc <- seq(-2, 4, 1)    # smaller range for cost
+cg <- seq(-2, 0, 0.5)  # smaller range for gamma
+
 
 tuning <- tune.svm(price_in_euro ~ power_kw + transmission_type + mileage_in_km + sporty + age_in_months + fuel_type_new + brand_type,
                    data=data.train_and_val,
                    scale = TRUE, type = "eps-regression", kernel = "radial",
                    gamma = 10^cg, cost = 2^cc, epsilon = 0.1,
-                   tunecontrol = tune.control(sampling = "cross",cross=5))
+                   tunecontrol = tune.control(sampling = "cross",cross=3))
 
 print(tuning)
 model <- tuning$best.model    # das Model mit optimalen Tuningparametern
