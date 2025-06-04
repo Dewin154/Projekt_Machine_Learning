a74521214455aea899a4f10e8af269bf44a4f5d9
Nico <ni.dilger@gmail.com>
2025-06-04 14:05:43 +0200

    jetzt gehts

--- NN_new.R
+++ NN_new.R
@@ -1,4 +1,7 @@
 library(ANN2)
+library(caret)
+
+set.seed(42)
 
 X <- model.matrix(
   price_in_euro ~ power_kw + transmission_type + mileage_in_km + sporty + age_in_months + fuel_type_new + brand_type,
@@ -7,39 +10,54 @@ X <- model.matrix(
 X <- X[, -1]
 y <- data.train_and_val[, "price_in_euro"]
 
-# Kreuzvalidierung mit ANN2
-set.seed(42)
-cv_res <- cv.neuralnetwork(
-  X,
-  y,
-  hidden.layers = list(c(3, 3), c(5, 3), c(7, 3)), # nur erster Layer variiert
-  regression = TRUE,
-  loss.type = "absolute",
-  learn.rates = 1e-04,
-  n.epochs = 20,
-  k.folds = 5,
-  verbose = TRUE
-)
-print(cv_res)
+# caret benötigt alles als Dataframe
+train_data <- as.data.frame(X)
+train_data$price_in_euro <- y
 
-# Optional: bestes Modell trainieren
-best_hidden <- cv_res$best.hidden.layers
-model <- neuralnetwork(
-  X,
-  y,
-  hidden.layers = best_hidden,
-  regression = TRUE,
-  loss.type = "absolute",
-  learn.rates = 1e-04,
-  n.epochs = 50,
-  verbose = FALSE
-)
-plot(model)
+# Definiere Trainingskontrolle für 10-fache Kreuzvalidierung
+ctrl <- trainControl(method = "cv", number = 10)
 
-# Prognosen auf Testdaten
+# Nur die Anzahl der Neuronen im ersten Layer als Parameter
+grid <- expand.grid(hidden1 = c(3, 5, 7))
+
+# Eigene Trainingsfunktion für caret
+caret_ann2 <- list(
+  type = "Regression",
+  library = "ANN2",
+  parameters = data.frame(parameter = "hidden1", class = "numeric", label = "Hidden Layer 1"),
+  grid = function(x, y, len = NULL, search = "grid") grid,
+  fit = function(x, y, wts, param, lev, last, classProbs, ...) {
+    neuralnetwork(
+      x, y,
+      hidden.layers = c(param$hidden1, 3),
+      regression = TRUE,
+      loss.type = "absolute",
+      learn.rates = 1e-04,
+      n.epochs = 50,
+      verbose = FALSE
+    )
+  },
+  predict = function(modelFit, newdata, submodels = NULL) {
+    as.vector(predict(modelFit, as.matrix(newdata))$predictions)
+  },
+  prob = NULL
+)
+
+# Training mit caret
+model <- train(
+  price_in_euro ~ .,
+  data = train_data,
+  method = caret_ann2,
+  trControl = ctrl,
+  tuneGrid = grid,
+  metric = "MAE"
+)
+print(model)
+
+# Prognose auf Testdaten
 X.test <- model.matrix(price_in_euro ~ power_kw + transmission_type + mileage_in_km + sporty + age_in_months + fuel_type_new + brand_type,
                        data.test)
 X.test <- X.test[, -1]
-prognosen <- predict(model, X.test)$predictions
+prognosen <- predict(model, as.data.frame(X.test))
 y.test <- data.test[, "price_in_euro"]
 mean(abs(y.test - prognosen))
